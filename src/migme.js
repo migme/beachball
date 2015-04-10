3599eb655733f3a09ef59dbda44c9932764c59f0
Sebastiaan Deckers <seb@ninja.sg>
2015-04-10 17:34:55 +0800

    Cute pattern for default argument values and package version

--- src/migme.js
+++ src/migme.js
@@ -1,7 +1,15 @@
 import Session from './lib/Session'
 import API from './lib/API'
+import {version} from '../package.json'
 
 export default class Migme {
+  constructor (opts) {
+    Object.assign(this, {
+      client_id = '',
+      redirect_uri = '',
+      version = version,
+      access_token = ''
+    }, opts)
 
     this.Session = new Session(this)
     this.API = new API(this)
