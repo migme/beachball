77227dabe4a8d1e8105b0159d7472334fc63aa51
Sebastiaan Deckers <seb@ninja.sg>
2015-06-02 17:58:10 +0800

    Less browsers for each test run

--- grunt/karma.js
+++ grunt/karma.js
@@ -63,26 +63,15 @@ var karma = {
 var customLaunchers = {
   sl_chrome: {
     base: 'SauceLabs',
-    browserName: 'chrome',
-    platform: 'Windows 7',
-    version: '35'
+    browserName: 'chrome'
   },
   sl_firefox: {
     base: 'SauceLabs',
-    browserName: 'firefox',
-    version: '30'
-  },
-  sl_ios_safari: {
-    base: 'SauceLabs',
-    browserName: 'iphone',
-    platform: 'OS X 10.9',
-    version: '7.1'
+    browserName: 'firefox'
   },
   sl_ie_11: {
     base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8.1',
-    version: '11'
+    browserName: 'internet explorer'
   }
 }
 
