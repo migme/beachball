2504d369241a064a9f93c0e833023cca65d6b071
Ed Moore <eddie.moore@gmail.com>
2015-07-22 13:51:16 +0800

    saucelabs

--- karma.config.js
+++ karma.config.js
@@ -1,5 +1,5 @@
-// var isparta = require('isparta')
-// var istanbul = require('browserify-istanbul')
+var assign = require('lodash.assign')
+var pkg = require('./package.json')
 var webpackConfig = require('./webpack.config')
 webpackConfig.devtool = 'inline-source-map'
 
@@ -63,26 +63,37 @@ var options = {
   browsers: ['Chrome', 'Firefox']
 }
 
-// if (!argv.coverage || argv.coverage === true) {
-//   options.browserify.transform.unshift(
-//     istanbul({
-//       instrumenter: isparta
-//     })
-//   )
-//   options.reporters.push('coverage')
-//   options.coverageReporter = {
-//     dir: 'coverage',
-//     reporters: [
-//       { type: 'lcovonly', subdir: '.', file: 'lcov.info' },
-//       { type: 'text' },
-//       { type: 'text-summary' }
-//     ]
-//   }
-// } else {
-//   options.browserify.transform.unshift(
-//     ['babelify', { stage: 0 } ]
-//   )
-// }
+var customLaunchers = {
+  // sl_android: {
+  //   base: 'SauceLabs',
+  //   browserName: 'android'
+  // },
+  sl_ie: {
+    base: 'SauceLabs',
+    browserName: 'internet explorer'
+  }
+  // sl_ipad: {
+  //   base: 'SauceLabs',
+  //   browserName: 'ipad'
+  // }
+}
+
+if (process.env.TRAVIS) {
+  assign(options, {
+    sauceLabs: {
+      testName: pkg.name
+    },
+    customLaunchers: customLaunchers,
+    browsers: Object.keys(customLaunchers),
+    reporters: ['spec', 'coverage', 'saucelabs']
+  })
+}
+
+if (process.env.CI_NAME === 'codeship') {
+  assign(options, {
+    reporters: ['spec', 'coverage']
+  })
+}
 
 module.exports = function (config) {
   config.set(options)
