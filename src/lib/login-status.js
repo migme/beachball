b53c2418db5dd5c935fd603ebaafdc29bf25790f
Ed Moore <eddie.moore@gmail.com>
2015-12-18 10:35:57 +0800

    If  no access token then check cookie

--- src/lib/login-status.js
+++ src/lib/login-status.js
@@ -1,8 +1,17 @@
 import localstorage from 'universal-localstorage'
 import config from '../config'
 
+function getSessionId (sKey) {
+  if (!sKey || typeof document === 'undefined') return null
+  const cookie = document.cookie.replace(new RegExp('(?:(?:^|.*;)\\s*' +
+   encodeURIComponent(sKey).replace(/[\-\.\+\*]/g, '\\$&') +
+   '\\s*\\=\\s*([^;]*).*$)|^.*$'), '$1')
+  const session_id = decodeURIComponent(cookie)
+  return session_id ? { session_id } : null
+}
+
 // Get Login Status
 export default async function () {
   const session = localstorage.getItem(config.storage_key)
-  return JSON.parse(session)
+  return JSON.parse(session) || getSessionId('eid')
 }
