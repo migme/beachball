22f85a1bd73df793cb053f777f62496a536c7331
Ed Moore <eddie.moore@gmail.com>
2015-12-18 10:36:18 +0800

    location doesn't have replaceState. history does

--- src/utils/hash.js
+++ src/utils/hash.js
@@ -2,5 +2,5 @@ export function trim () {
   const location = window.location
   const length = location.href.length - location.hash.length
   const trimmed = location.href.substring(0, length)
-  location.replaceState(null, null, trimmed)
+  window.history.replaceState(null, null, trimmed)
 }
