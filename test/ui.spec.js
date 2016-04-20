9932cc0f1e0b706c66437aec98d563ad8401be92
Andre Lee <andretw@gmail.com>
2016-04-20 14:30:54 +0800

    WW-2013: Added test cases for MIGME.ui.

--- test/ui.spec.js
+++ test/ui.spec.js
@@ -4,14 +4,30 @@ import sinon from 'sinon'
 import sinonChai from 'sinon-chai'
 import chaiAsPromised from 'chai-as-promised'
 import Beachball from '../src'
-import config from '../src/config'
 
 chai.use(sinonChai)
 chai.use(chaiAsPromised)
 
-const baseUrl = 'https://localhost'
-const access_token = '1234567890'
-Object.assign(config, {
-  baseUrl,
-  access_token
+describe('UI', () => {
+  it('should be a function', () => {
+    expect(Beachball.ui).to.be.a('function')
+  })
+
+  if (typeof window !== 'undefined') {
+    describe('sharing post will open a popup window', () => {
+      beforeEach(() => {
+        sinon.stub(window, 'open')
+      })
+      it('should open a window', () => {
+        const params = {method: 'share', href: 'https://alivenotdead.com'}
+        Beachball.ui(params)
+        expect(window.open).to.have.been.calledWith(
+          'https://mig.me/share_to_mig33?referrer=&campaign=&return_url=&href=http%3A%2F%alivenotdead.com'
+        )
+      })
+      afterEach(() => {
+        window.open.restore()
+      })
+    })
+  }
 })
