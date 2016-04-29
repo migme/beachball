257bac3004069d6b763beedc79a562c90fe1c084
Andre Lee <andretw@gmail.com>
2016-04-29 12:22:17 +0800

    Re-allocated the location of the new pop window.

--- src/lib/ui.js
+++ src/lib/ui.js
@@ -18,9 +18,21 @@ const uiMethods = async({ method, href } = {}) => {
 }
 
 function openWindow (href) {
+  const w = 500
+  const h = 500
   const pathForShareToMigme = '/share_to_migme?referrer=&campaign=&return_url=&href='
   const url = `${config.host}${pathForShareToMigme}${href}`
-  return window.open(url, '', 'height=500px, width=500px')
+
+  const dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+  const dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+  const width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+  const height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+  var top = ((height / 2) - (h / 2)) + dualScreenTop;
+
+  return window.open(url, 'migme', `height=${h}, width=${w}, top=${top}, left=${left}`)
 }
 
 // UI
